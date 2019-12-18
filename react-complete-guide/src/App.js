c72e3460cc78a2883747a501fce0e97190ed2726
Sanaz <sanaz.azh@gmail.com>
2019-12-18 06:25:31 -0500

    add state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Bob", age: 34 },
+      { name: "Kat", age: 3 },
+      { name: "Stephanie", age: 26 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi! I'm a react Component</h1>
-        <Person name="Bob" age="34" />
-        <Person name="Kat" age="3">My hobbies: racing</Person>
+        <button>Switch Names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
