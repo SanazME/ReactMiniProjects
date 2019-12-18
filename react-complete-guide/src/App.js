e44e00c3c8b6d0fe4d5ecfe70dc5f41b227dc5bb
Sanaz <sanaz.azh@gmail.com>
2019-12-18 07:04:51 -0500

    change state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,11 +11,21 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "Bobbing", age: 34 },
+        { name: "Kat", age: 3 },
+        { name: "Stephanie", age: 350 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi! I'm a react Component</h1>
-        <button>Switch Names</button>
+        <button onClick={this.switchNameHandler} >Switch Names</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
