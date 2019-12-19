1caa967f22bfc4385a1fb529694733b9af8d18f1
Sanaz <sanaz.azh@gmail.com>
2019-12-19 05:52:52 -0500

    complete state in class-based component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,7 +8,15 @@ class App extends Component {
       { name: "Bob", age: 34 },
       { name: "Kat", age: 3 },
       { name: "Stephanie", age: 26 }
-    ]
+    ],
+
+    clicked: true
+  }
+
+  toggleHandler = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    })
   }
 
   switchNameHandler = () => {
@@ -26,6 +34,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi! I'm a react Component</h1>
         <button onClick={this.switchNameHandler} >Switch Names</button>
+        <button onClick={this.toggleHandler} >{this.state.clicked ? 'ON' : 'OFF'}</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
