fe5bec8a29dd7d280a53980a08f6842c80d9e12f
Sanaz <sanaz.azh@gmail.com>
2020-01-16 06:48:16 -0500

    add assignmet 2 - part 1

---
+++ practise2/assignment2/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state = {
+    inputText: '',
+    inputLength: 0
+  }
+
+  outputLengthHandler = (event) => {
+    const inputText = event.target.value;
+    const inputLength = inputText.length;
+
+    // Update state
+    this.setState({
+      inputText: inputText,
+      inputLength: inputLength
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        Input Text :
+        <input type="text" onChange={this.outputLengthHandler} value={this.state.inputText} />
+        <h2>The length of the input text is : {this.state.inputLength}</h2>
+      </div>
+    )
+  }
+}
