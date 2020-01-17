fab999e95bf183dceb48b29256c8e104cfc8e54d
Sanaz <sanaz.azh@gmail.com>
2020-01-17 06:49:39 -0500

    add assignmet 2 - part 4&5

--- practise2/assignment2/src/App.js
+++ practise2/assignment2/src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
 import ValidationComponent from './ValidationComponent/ValidationComponent'
+import CharComponent from './CharComponent/CharComponent'
 
 export default class App extends Component {
   state = {
     inputText: '',
-    inputLength: 0
+    inputLength: 0,
+    charArray: []
   }
 
   outputLengthHandler = (event) => {
     const inputText = event.target.value;
     const inputLength = inputText.length;
+    const charArray = inputText.split(/\s+/);
+    // const charArray = inputText.split('');
 
     // Update state
     this.setState({
       inputText: inputText,
-      inputLength: inputLength
+      inputLength: inputLength,
+      charArray: charArray
+    })
+  }
+
+  deleteCharHandler = (index) => {
+    const charArrCopy = [...this.state.charArray];
+    charArrCopy.splice(index, 1);
+    this.setState({
+      charArray: charArrCopy
     })
   }
 
@@ -28,6 +41,27 @@ export default class App extends Component {
         <ValidationComponent
           length={this.state.inputLength}
         />
+
+        <div>
+          <ul>
+            {/* {this.state.inputText.split('').map((char, index) => { */}
+            {this.state.charArray.map((char, index) => {
+              return (
+                <CharComponent
+                  char={char}
+                  click={() => this.deleteCharHandler(index)}
+                />
+              )
+            })}
+            {/* {this.state.inputText.split(/\s+/).map(word => {
+              return (
+                <CharComponent
+                  char={word.trim()}
+                />
+              )
+            })} */}
+          </ul>
+        </div>
       </div>
     )
   }
