fa35ebb419f1bdec720a9c9c6a4b78e5f56964d6
Sanaz <sanaz.azh@gmail.com>
2020-02-04 06:59:19 -0500

    use refs in html element and react component

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -7,13 +7,28 @@ import PropTypes from 'prop-types';
 // import Aux from '../../../hoc/Aux'
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] is rendering ...')
         return (
             <Fragment>
                 <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
                 <p key="i2">{this.props.children}</p>
-                <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
+                <input
+                    ref={this.inputElementRef}
+                    key="i3"
+                    type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name}
+                />
             </Fragment>
         )
     }
