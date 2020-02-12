e6bcec92cb8fab3f3ad2583642a02aca56862552
Sanaz <sanaz.azh@gmail.com>
2020-02-12 06:23:34 -0500

    Add burger ingrediants

---
+++ burger_builder/src/components/Layout/Burger/BurgerIngrediant/BurgerIngrediant.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import classes from './BurgerIngrediant.module.css'
+import propTypes from 'prop-types';
+
+export default function burgerIngrediant(props) {
+    let ingrediant = null;
+
+    switch (props.type) {
+        case ('bread-bottom'):
+            ingrediant = <div className={classes.BreadBottom}></div>
+            break;
+
+        case ('bread-top'):
+            ingrediant = (
+                <div className={classes.BurgerTop}>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
+                </div>
+            );
+            break;
+
+        case ('meat'):
+            ingrediant = <div className={classes.Meat}></div>
+            break;
+
+        case ('cheese'):
+            ingrediant = <div className={classes.Cheese}></div>
+            break;
+
+        case ('salad'):
+            ingrediant = <div className={classes.Salad}></div>
+            break;
+
+        case ('bacon'):
+            ingrediant = <div className={classes.Bacon}></div>
+            break;
+        default:
+            ingrediant = null;
+    };
+
+    return ingrediant
+
+    burgerIngrediant.propTypes = {
+        type: propType.string.isRequired
+    }
+};
