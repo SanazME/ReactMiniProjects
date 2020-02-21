c1e82298b2dba601b125ddc62c93a17dcfe5922f
Sanaz <sanaz.azh@gmail.com>
2020-02-21 06:32:10 -0500

    Add button component with a dynamic style

--- burger_builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger_builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,6 +19,8 @@ const OrderSummary = (props) => {
                 {ingrediantSummary}
             </ul>
             <p>Continue to checkout?</p>
+            <button>CANCEL</button>
+            <button>CONTINUE</button>
 
         </Fragment>
     );
