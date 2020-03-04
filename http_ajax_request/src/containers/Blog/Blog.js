b822b58d65379d9fa0583715a435ea2c3b5cf9d0
Sanaz <sanaz.azh@gmail.com>
2020-03-04 06:43:00 -0500

    Fetch data for selected post - part 1

--- http_ajax_request/src/containers/Blog/Blog.js
+++ http_ajax_request/src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -31,12 +32,19 @@ class Blog extends Component {
             })
     }
 
+    postSelectedHandler = (postId) => {
+        this.setState({
+            selectedPostId: postId,
+        })
+    }
+
     render() {
         const posts = this.state.posts.map(post => {
             return <Post
                 key={post.id}
                 title={post.title}
                 author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
             />
         })
 
@@ -47,7 +55,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
